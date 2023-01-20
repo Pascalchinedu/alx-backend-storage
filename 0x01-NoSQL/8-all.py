571f63fd60102955cea4b0ecb04a60d0065347e9
Pascal <pascalchinedu2000@gmail.com>
2023-01-20 10:06:09 +0000

    Update Script

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Contains function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    Args:
+        mongo_collection: Mongo db collection name
+    Returns:
+        Documents found in collection
+    """
+    return mongo_collection.find()
